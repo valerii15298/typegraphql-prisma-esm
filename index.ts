4bc51ebdbd2009c664d2c6f58685d5771942c481
Valerii Petryniak <vpetryniak@fxcintel.com>
2023-03-27 17:17:33 +0300

    error repro

---
+++ index.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import { buildSchema, FieldResolver, Resolver } from "type-graphql";
+import path from "path";
+import { Prisma } from "@prisma/client";
+
+import { resolvers, User } from "@generated/type-graphql";
+import { JSONResolver } from "graphql-scalars";
+
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+@Resolver((_of) => User)
+class CustomResolver {
+  @FieldResolver((_type) => JSONResolver, {
+    nullable: true,
+  })
+  async data(): Promise<Prisma.JsonValue[]> {
+    return ["1", "2", "3"];
+  }
+}
+
+buildSchema({
+  resolvers: [...resolvers, CustomResolver],
+  emitSchemaFile: path.resolve(__dirname, "./generated-schema.graphql"),
+  validate: false,
+});
